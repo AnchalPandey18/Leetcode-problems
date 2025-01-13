aebabb26b67f0a1a9a5b69ce62637b3214bdbc34
AnchalPandey18 <AnchalPandey1801@gmail.com>
2025-01-13 13:49:17 +0530

    Time: 24 ms (59.68%), Space: 31.9 MB (86.69%) - LeetHub

---
+++ 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minimumLength(string s) {
+        int n = s.size();
+        vector<int>freq(26,0);
+        int deleted = 0;
+        for(int i=0;i<n;i++){
+            freq[s[i]-'a'] +=1;
+
+            if(freq[s[i]-'a']==3){
+                freq[s[i]-'a'] -= 2;
+                deleted +=2;
+            }
+        }
+        return n-deleted;
+    }
+};
