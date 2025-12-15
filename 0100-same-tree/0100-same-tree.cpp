072da02b6ba95886ab1a2872b3a457c8ea18596c
AnchalPandey18 <AnchalPandey1801@gmail.com>
2025-12-15 18:05:48 +0530

    Time: 0 ms (100%), Space: 12.9 MB (47.06%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -11,13 +11,33 @@
  */
 class Solution {
 public:
-    bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(!p && !q){
-            return true;
-        }
-        if(p && q) {
-            return (p ->val == q ->val) && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
-        }
+//     bool isSameTree(TreeNode* p, TreeNode* q) {
+//         if(!p && !q){
+//             return true;
+//         }
+//         if(p && q) {
+//             return (p ->val == q ->val) && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+//         }
+//         return false;
+//     }
+ bool isSameTree(TreeNode* p, TreeNode* q) {
+    queue<pair<TreeNode*,TreeNode*>>qu;
+    qu.push({p,q});
+    while(!qu.empty()) {
+        auto node = qu.front();
+        qu.pop();
+
+        TreeNode* n1 = node.first;
+        TreeNode* n2 = node.second;
+
+        if(n1 == NULL & n2 == NULL)
+        continue;
+        if(n1 == NULL || n2 == NULL || n1->val != n2->val)
         return false;
+
+        qu.push({n1->left,n2->left});
+        qu.push({n1->right,n2->right});
     }
+    return true;
+ }
 };
